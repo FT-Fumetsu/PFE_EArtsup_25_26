4270d1c795174896f540d726451910257d7cf59a
FTFumetsu <esmieudamien18@gmail.com>
2025-12-10 13:08:38 +0100

    Add Effect, Cooldown and Widget for each ability

--- Source/PFE_EArtsup_25_26/PFE_EArtsup_25_26.Build.cs
+++ Source/PFE_EArtsup_25_26/PFE_EArtsup_25_26.Build.cs
@@ -8,9 +8,9 @@ public class PFE_EArtsup_25_26 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
