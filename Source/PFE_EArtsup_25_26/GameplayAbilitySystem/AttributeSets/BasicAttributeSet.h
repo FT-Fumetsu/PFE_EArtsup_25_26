e795d8b030ce58c50a8b7180bdfd6454abe23e99
FTFumetsu <esmieudamien18@gmail.com>
2025-12-19 12:11:56 +0100

    Test Charge ATtack but doesn't work so much

--- Source/PFE_EArtsup_25_26/GameplayAbilitySystem/AttributeSets/BasicAttributeSet.h
+++ Source/PFE_EArtsup_25_26/GameplayAbilitySystem/AttributeSets/BasicAttributeSet.h
@@ -40,6 +40,14 @@ public:
 	FGameplayAttributeData Levels;
 	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, Levels);
 	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData ChargeTime;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, ChargeTime);
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData MaxChargeTime;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, MaxChargeTime);
+	
 public:
 	
 	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
