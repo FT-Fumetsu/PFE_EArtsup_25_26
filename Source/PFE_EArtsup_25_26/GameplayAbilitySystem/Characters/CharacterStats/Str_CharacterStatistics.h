08fce6dfa593805a28e3cc488e588fd6d43c22dc
FTFumetsu <esmieudamien18@gmail.com>
2026-04-30 15:52:50 +0200

    Fix Dummies

--- Source/PFE_EArtsup_25_26/GameplayAbilitySystem/Characters/CharacterStats/Str_CharacterStatistics.h
+++ Source/PFE_EArtsup_25_26/GameplayAbilitySystem/Characters/CharacterStats/Str_CharacterStatistics.h
@@ -12,27 +12,27 @@ struct FStr_CharacterStatistics
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Stats")
 	int MaxHealthValue = 100;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Stats")
 	float RunSpeedValue = 500.f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Stats")
 	float LifeStealValue = 0.f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Stats")
 	float ArmorValue = 0.f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Stats")
 	float AttackSpeedValue = 0.f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Stats")
 	float AttackValue = 0.f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Stats")
 	float DashDistanceValue = 600.f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Stats")
 	float DashTimeValue = 0.2f;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Stats")
 	float DashCooldownValue = 1.f;
 };
